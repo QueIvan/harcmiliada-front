f2f805778260f5b2881c79a5a8557a21ca2ee37f
Jan Szewczyk <36161455+IvanPlease@users.noreply.github.com>
2021-10-24 15:22:59 +0200

    [Init] Commit

--- src/index.js
+++ src/index.js
@@ -4,22 +4,16 @@ import './index.css';
 import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter>
-    <Auth0Provider
-      domain="dev-6x5bsznk.us.auth0.com"
-      clientId="w9oZIPZyRhMrvdaGo0xwf585KtwtfuBT"
-      redirectUri={window.location.origin+"/dashboard"}
-    >
-      <App />
-    </Auth0Provider>
-  </HashRouter>,
+  <Auth0Provider
+    domain="dev-6x5bsznk.us.auth0.com"
+    clientId="w9oZIPZyRhMrvdaGo0xwf585KtwtfuBT"
+    redirectUri={window.location.origin+"/dashboard"}
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
