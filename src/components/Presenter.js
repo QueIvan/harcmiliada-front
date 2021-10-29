05bf5b0d73233c0e7de9e22b42e7031a20f25dcb
Jan Szewczyk <36161455+IvanPlease@users.noreply.github.com>
2021-10-29 12:04:18 +0200

    [Init] Commit

--- src/components/Presenter.js
+++ src/components/Presenter.js
@@ -14,6 +14,7 @@ const BackBox = styled(MuiBox)(({ theme }) => ({
   display: "flex",
   backgroundColor: theme.palette.board.bg,
   overflow: "hidden",
+  alignItems: "center",
   justifyContent: "center",
 }));
 
